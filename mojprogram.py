cc7d39ac73344618b2feb656aab70f52cfc87319
ania152 <ania152@users.noreply.github.com>
2019-12-07 12:17:52 +0100

    msg

--- mojprogram.py
+++ mojprogram.py
@@ -1,2 +1,15 @@
 def zwroc_100():
-    return 100
+    return 100 #jesli tu wpisze x to musze wpisac obojetnie jaki parametr, np 0 w drugim pliku
+
+
+def zwroc_parametr(x):
+    return 124
+
+def helloworld(x):
+    return "hello world"
+def dodaj(x, y):
+    return x + y
+def odejmij(x, y):
+    return x - y
+def pomnoz(x, y):
+    return x * y
