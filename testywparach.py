e0126652ed9b1edd01301c603c1bda1f9635bc73
ania152 <ania152@users.noreply.github.com>
2019-12-07 13:33:48 +0100

    cos

---
+++ testywparach.py
@@ -0,0 +1,17 @@
+import unittest
+import program2
+class TestTDD1(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual(program2.upper("ania"), "ANIA")
+    def test_lower(self):
+        self.assertEqual(program2.lower("KOT"), "kot")
+    def test_replace(self):
+        self.assertEqual(program2.replace("EGG"), "EGGS")
+    def test_join(self):
+        self.assertEqual(program2.join("EGG", "S"), "EGGS")
+    def test_len(self):
+        self.assertEqual(program2.len("EGGS"), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
